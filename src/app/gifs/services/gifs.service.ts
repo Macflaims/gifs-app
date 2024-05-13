a90bbfb48b04b6811df384aee9cc0dec027ce245
Martin <martinsanchez2533@gmail.com>
2024-05-13 10:43:59 -0300

    agrega componente gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,7 +6,10 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
   providedIn: 'root'
 })
 export class GifsService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+
+  }
 
   public gifList: Gif[] = []
 
@@ -25,7 +28,19 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this.saveLocalStorage();
 
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem("history", JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem("history")) return
+    this._tagsHistory = JSON.parse(localStorage.getItem("history")!)
+    if (this._tagsHistory.length === 0) return
+    this.searchTag(this._tagsHistory[0])
   }
 
   searchTag(tag: string): void {
